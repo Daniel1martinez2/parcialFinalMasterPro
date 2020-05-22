5777b307906d54ef63d3561244c8c1c749fd46ff
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-05-22 07:50:18 -0500

    second commit just prepare al the things

---
+++ src/view/Main.java
@@ -0,0 +1,23 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+	 PApplet.main("view.Main");
+
+	}
+	public void settings() {
+
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+	}
+
+}
